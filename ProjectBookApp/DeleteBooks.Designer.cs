fe7756fe53b22579d736cb2d6dca5d3678267522
Anas-Abdul-El <anasabdulel@gmail.com>
2026-05-22 18:15:29 +0300

    intial commit

---
+++ ProjectBookApp/DeleteBooks.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace ProjectBookApp
+{
+    partial class DeleteBooks
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DeleteBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "DeleteBooks";
+            this.Size = new System.Drawing.Size(1025, 696);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ ProjectBookApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectBookApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void LoadControl(UserControl control)
+        {
+            panelMain.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            panelMain.Controls.Add(control);
+        }
+
+        private void btn_add_books_panel_Click(object sender, EventArgs e)
+        {
+            LoadControl(new AddBooks());
+        }
+
+        private void btn_update_panel_Click(object sender, EventArgs e)
+        {
+            LoadControl(new UpdateBooks());
+        }
+
+        private void btn_del_panel_Click(object sender, EventArgs e)
+        {
+            LoadControl(new DeleteBooks());
+        }
+
+        private void reports_btn_panel_Click(object sender, EventArgs e)
+        {
+            LoadControl(new Report());
+        }
+    }
+}
