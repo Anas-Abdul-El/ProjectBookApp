2adfca62b3bf0f79f03f27d900df5920292d33d2
Anas-Abdul-El <anasabdulel@gmail.com>
2026-05-23 13:34:57 +0300

    create the AddBook userController layout

--- ProjectBookApp/AddBooks.Designer.cs
+++ ProjectBookApp/AddBooks.Designer.cs
@@ -28,19 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(121, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(243, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(121, 253);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(243, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Quantity";
             // 
             // AddBooks
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "AddBooks";
-            this.Size = new System.Drawing.Size(1203, 666);
+            this.Size = new System.Drawing.Size(381, 405);
             this.Load += new System.EventHandler(this.AddBooks_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ProjectBookApp/Form1.Designer.cs
+++ ProjectBookApp/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn_add_books_panel = new System.Windows.Forms.Button();
-            this.btn_update_panel = new System.Windows.Forms.Button();
-            this.btn_del_panel = new System.Windows.Forms.Button();
             this.reports_btn_panel = new System.Windows.Forms.Button();
+            this.btn_del_panel = new System.Windows.Forms.Button();
+            this.btn_update_panel = new System.Windows.Forms.Button();
+            this.btn_add_books_panel = new System.Windows.Forms.Button();
             this.panelMain = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
@@ -49,60 +49,8 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(145, 626);
+            this.panel1.Size = new System.Drawing.Size(145, 470);
             this.panel1.TabIndex = 0;
-            // 
-            // btn_add_books_panel
-            // 
-            this.btn_add_books_panel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_add_books_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btn_add_books_panel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.btn_add_books_panel.FlatAppearance.BorderSize = 0;
-            this.btn_add_books_panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_add_books_panel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_add_books_panel.Location = new System.Drawing.Point(0, 60);
-            this.btn_add_books_panel.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_add_books_panel.Name = "btn_add_books_panel";
-            this.btn_add_books_panel.Size = new System.Drawing.Size(145, 57);
-            this.btn_add_books_panel.TabIndex = 2;
-            this.btn_add_books_panel.Text = "Add Books";
-            this.btn_add_books_panel.UseVisualStyleBackColor = false;
-            this.btn_add_books_panel.Click += new System.EventHandler(this.btn_add_books_panel_Click);
-            // 
-            // btn_update_panel
-            // 
-            this.btn_update_panel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_update_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btn_update_panel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.btn_update_panel.FlatAppearance.BorderSize = 0;
-            this.btn_update_panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_update_panel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_update_panel.Location = new System.Drawing.Point(0, 117);
-            this.btn_update_panel.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_update_panel.Name = "btn_update_panel";
-            this.btn_update_panel.Size = new System.Drawing.Size(145, 59);
-            this.btn_update_panel.TabIndex = 3;
-            this.btn_update_panel.Text = "Update Books";
-            this.btn_update_panel.UseVisualStyleBackColor = false;
-            this.btn_update_panel.Click += new System.EventHandler(this.btn_update_panel_Click);
-            // 
-            // btn_del_panel
-            // 
-            this.btn_del_panel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_del_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btn_del_panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btn_del_panel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.btn_del_panel.FlatAppearance.BorderSize = 0;
-            this.btn_del_panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_del_panel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_del_panel.Location = new System.Drawing.Point(0, 176);
-            this.btn_del_panel.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_del_panel.Name = "btn_del_panel";
-            this.btn_del_panel.Size = new System.Drawing.Size(145, 59);
-            this.btn_del_panel.TabIndex = 4;
-            this.btn_del_panel.Text = "Delete Book";
-            this.btn_del_panel.UseVisualStyleBackColor = false;
-            this.btn_del_panel.Click += new System.EventHandler(this.btn_del_panel_Click);
             // 
             // reports_btn_panel
             // 
@@ -122,20 +70,72 @@
             this.reports_btn_panel.UseVisualStyleBackColor = false;
             this.reports_btn_panel.Click += new System.EventHandler(this.reports_btn_panel_Click);
             // 
+            // btn_del_panel
+            // 
+            this.btn_del_panel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_del_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_del_panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_del_panel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_del_panel.FlatAppearance.BorderSize = 0;
+            this.btn_del_panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_del_panel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_del_panel.Location = new System.Drawing.Point(0, 176);
+            this.btn_del_panel.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_del_panel.Name = "btn_del_panel";
+            this.btn_del_panel.Size = new System.Drawing.Size(145, 59);
+            this.btn_del_panel.TabIndex = 4;
+            this.btn_del_panel.Text = "Delete Book";
+            this.btn_del_panel.UseVisualStyleBackColor = false;
+            this.btn_del_panel.Click += new System.EventHandler(this.btn_del_panel_Click);
+            // 
+            // btn_update_panel
+            // 
+            this.btn_update_panel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_update_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_update_panel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_update_panel.FlatAppearance.BorderSize = 0;
+            this.btn_update_panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_update_panel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_update_panel.Location = new System.Drawing.Point(0, 120);
+            this.btn_update_panel.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_update_panel.Name = "btn_update_panel";
+            this.btn_update_panel.Size = new System.Drawing.Size(145, 59);
+            this.btn_update_panel.TabIndex = 3;
+            this.btn_update_panel.Text = "Update Books";
+            this.btn_update_panel.UseVisualStyleBackColor = false;
+            this.btn_update_panel.Click += new System.EventHandler(this.btn_update_panel_Click);
+            // 
+            // btn_add_books_panel
+            // 
+            this.btn_add_books_panel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_add_books_panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_add_books_panel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_add_books_panel.FlatAppearance.BorderSize = 0;
+            this.btn_add_books_panel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_add_books_panel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_add_books_panel.Location = new System.Drawing.Point(-3, 69);
+            this.btn_add_books_panel.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_add_books_panel.Name = "btn_add_books_panel";
+            this.btn_add_books_panel.Size = new System.Drawing.Size(145, 57);
+            this.btn_add_books_panel.TabIndex = 2;
+            this.btn_add_books_panel.Text = "Add Books";
+            this.btn_add_books_panel.UseVisualStyleBackColor = false;
+            this.btn_add_books_panel.Click += new System.EventHandler(this.btn_add_books_panel_Click);
+            // 
             // panelMain
             // 
             this.panelMain.Controls.Add(this.label1);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Location = new System.Drawing.Point(145, 0);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(1021, 626);
+            this.panelMain.Size = new System.Drawing.Size(789, 470);
             this.panelMain.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(255, 69);
+            this.label1.Location = new System.Drawing.Point(146, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(436, 107);
             this.label1.TabIndex = 0;
@@ -146,7 +146,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(1166, 626);
+            this.ClientSize = new System.Drawing.Size(934, 470);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
